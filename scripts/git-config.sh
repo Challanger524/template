9b3525e2dd0393ed143cc0f98dbd29851f74bf62
Daniil Semenenko <Daniilsemen@gmail.com>
2022-02-20 21:27:37 +0200

    Impl include of a user '.gitconfig' at project's parent folder #tpl_p #Script

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,6 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Configuring '.git/config' file"
 
-## Include
+## Include Project scope
 git config --local --add include.path "../.gitconfig"
+
+
+
+## Include User settings (must be a last include)
+GITCONFIG="`git rev-parse --show-toplevel`/../.gitconfig"
+if [[ -f "$GITCONFIG" ]]; then
+	echo "Include user's git settings: '$GITCONFIG'"
+	git config --local --add include.path "$GITCONFIG"
+fi
+# File end: Do not insert after "Include User settings"!
